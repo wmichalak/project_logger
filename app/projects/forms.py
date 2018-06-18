b7226c92aca1dc376c6b22dc28ccd6f29fee1d41
William Michalak <wmichalak@siluriatech.com>
2018-06-18 11:35:03 -0700

    Add queryselectfield to access employe information and pre-fill the drop down.

--- app/projects/forms.py
+++ app/projects/forms.py
@@ -14,14 +14,11 @@ class EntryForm(FlaskForm):
     Form for users to submit project document into log
     """
 
-    employee = StringField('employee', validators=[DataRequired()]) #QuerySelectField(query_factory=lambda: Employee.query.all(),get_label="name")
-    department = StringField('department', validators=[DataRequired()]) #QuerySelectField(query_factory=lambda: Department.query.all(),get_label="name")
+    employee = QuerySelectField(query_factory=lambda: Employee.query.all(),get_label="username", allow_blank=True)
     document_type = SelectField('Document type', choices=[('SOW','Scope of work'),('IER','Individual Experiment Report'),
-                                                          ('TPS','Technology Progress Summary'),('TES','Technology Evaluation Summary')])
-    description = StringField('Description', validators=[DataRequired()])
-    date = DateTimeField('Date',format="%Y-%m-%dT%H:%M:%S",
-                         default=datetime.today,
-                         validators=[DataRequired()])
+                                                          ('TPS','Technology Progress Summary'),('TER','Technology Evaluation Report')])
+    name = StringField('Project name', validators=[DataRequired()])
+    date = DateTimeField('Date',format="%Y_%m_%d",default=datetime.today,validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
