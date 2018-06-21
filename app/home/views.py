addfd2f678f92c853d05be2c58e0bd3b987c747e
William Michalak <wmichalak@siluriatech.com>
2018-06-20 21:30:51 -0700

    allowed for table ordering

--- app/home/views.py
+++ app/home/views.py
@@ -1,9 +1,10 @@
 # app/home/views.py
 
-from flask import abort, render_template
+from flask import abort, render_template, request
 from flask_login import current_user, login_required
 
 from . import home
+from ..models import Project
 
 @home.route('/')
 def homepage():
@@ -12,13 +13,26 @@ def homepage():
     """
     return render_template('home/index.html', title="Siluria Technologies")
 
-@home.route('/dashboard')
+@home.route('/dashboard/<orderby>')
 @login_required
-def dashboard():
+def dashboard(orderby = None):
     """
     Render the dashboard template on the /dashboard route
     """
-    return render_template('home/dashboard.html', title="Dashboard")
+    print orderby
+    if orderby == 'date':
+        projects = Project.query.order_by(Project.date.desc()) #all()
+    elif orderby == 'ID':
+        projects = Project.query.all()
+    elif orderby == 'project_name':
+        projects = Project.query.order_by(Project.doc_name)
+    elif orderby == 'owner':
+        projects = Project.query.order_by(Project.employee)
+    elif orderby == 'document':
+        projects = Project.query.order_by(Project.document_type)
+
+
+    return render_template('home/dashboard.html', projects=projects, title="Dashboard")
 
 @home.route('/admin/dashboard')
 @login_required
@@ -26,5 +40,5 @@ def admin_dashboard():
     # prevent non-admins from accessing the page
     if not current_user.is_admin:
         abort(403)
-
-    return render_template('home/admin_dashboard.html', title="Dashboard")
+    projects = Project.query.all()
+    return render_template('home/admin_dashboard.html', projects=projects, title="Dashboard")
